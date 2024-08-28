bff1e74aa3b69441fefa60b8b01ac55c2682d959
Vijay Maurya <115324275+Vijay6265@users.noreply.github.com>
2024-08-28 07:35:38 +0000

    Arrays

---
+++ level_02/exercise01.js
@@ -0,0 +1,43 @@
+// Array
+
+const myArr = [0,1,2,3,5,4];
+// console.log(myArr)
+// myArr.push(8);
+// myArr.push(9);
+// console.log(myArr)
+// // myArr.pop();
+// // console.log(myArr)
+// let slicearr = myArr.slice(1,3);
+// console.log(slicearr)
+
+// console.log(myArr.length)
+// console.log(myArr.indexOf(5))
+
+let cars = ['Maruti', 'Bolero', 'BMW', 'Ferari'];
+let bikes =['Splender', 'Tvs', 'Starcity', 'Pulsar'];
+// cars.push(bikes);
+// console.log(cars);
+
+// let bike_and_cars = cars.concat(bikes)
+// console.log(bike_and_cars) 
+
+// cars.copyWithin(bikes);
+// console.log(cars);
+
+// let all_vehicle = [...cars, ...bikes];
+// console.log(all_vehicle);
+
+// const another_array = [1,3,5,[2,6,6,7,8,[7,1,3,8],4],7,9];
+// const printArray = another_array.flat(Infinity);
+// console.log(printArray)
+
+
+// console.log(Array.isArray("Vijay"));
+// console.log(Array.from("Vijay"));
+// console.log(Array.from({name:"Vijay"}));
+
+
+let mark1 = 56;
+let mark2 = 60;
+let mark3 = 75;
+console.log(Array.of(mark1,mark2,mark3));
