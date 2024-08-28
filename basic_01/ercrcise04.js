87f938bc3fe89dd19aafac292c96d88d6a043b6c
Vijay Maurya <115324275+Vijay6265@users.noreply.github.com>
2024-08-28 06:32:41 +0000

    math_and_date

---
+++ basic_01/ercrcise04.js
@@ -0,0 +1,32 @@
+// Math library
+
+// console.log(Math);
+// console.log(Math.abs(-54));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+
+//  console.log(Math.random() * 10)
+//  console.log(Math.floor(Math.random() * 10));
+
+
+ // Date Object
+
+ let myDate = new Date();
+ // console.log(myDate);
+ // console.log(myDate.getMonth());
+ // const moonLanding = new Date('July 20, 69 00:20:18');
+ // console.log(moonLanding.getMonth())
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toLocaleString());
+ console.log(typeof myDate);
+ 
+ 
+ // const numbers = [5, 6, 2, 3, 7];
+ // const max = Math.max.apply(null, numbers);
+ // console.log(max);
+ // const min = Math.min.apply(null, numbers);
+ // console.log(min);
+ 
+ 
