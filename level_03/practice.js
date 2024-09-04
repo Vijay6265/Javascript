f5570250d56d98b596594dad1bcb07d40003168c
Vijay Maurya <115324275+Vijay6265@users.noreply.github.com>
2024-09-04 14:36:13 +0000

    practice

---
+++ level_03/practice.js
@@ -0,0 +1,31 @@
+// var arr = [1,2,3,6,4,7]
+// console.log(arr);
+
+// var arr = new Array(3);
+// arr[0]=1
+// arr[1]=2
+// arr[2]=3
+// console.log(arr)
+
+
+// let arr = [
+//     {
+//         fname:"Vijay",
+//         age:24
+//     },
+//     {
+//         fname:"Manju",
+//         age:23
+//     }
+// ]
+// console.log(typeof arr)
+
+
+
+// function fn(n){
+//     return n+1;
+// }
+// var num=[1,23,4,5,6,7,9]
+// var arr=num.map(fn);
+// console.log(arr)
+
